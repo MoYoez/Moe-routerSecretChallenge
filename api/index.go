c08e9c9409668fd7e67d6f350667c80ccd6eaa0b
MoYoez <i@himoyo.cn>
2023-08-16 22:12:49 +0800

    fix text err

--- api/index.go
+++ api/index.go
@@ -11,7 +11,7 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("tips", "Some Water Dropped On the Text, Can you help me to clean it?")
 	w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "application/json")
-	secretPath1 := "Achieved The First Goal. | Router To next: Time"
-	encText := wt.AddWaterMarkToText("To identify the world, You should consider more what I want to give.", secretPath1)
+	secretPath1 := "Achieved The First Goal. | Router To next: Time | Requests Args: ?timeunix=?"
+	encText := wt.AddWaterMarkToText("To identify the world", secretPath1)
 	fmt.Fprintf(w, encText+"\n"+"You should consider more what I want to give.")
 }
